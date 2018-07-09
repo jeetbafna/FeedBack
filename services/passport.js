00ea4e864d22cd2973238471de20a047984ef0be
jeet bafna <jeetanil97@gmail.com>
2018-07-09 16:28:54 +0530

    Authentication and refactoring

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+	.then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
@@ -8,8 +22,17 @@ passport.use(new GoogleStrategy({
 	callbackURL: '/auth/google/callback'
  }, (accessToken, refreshToken, profile, done) =>
  {
- 	console.log('accessToken', accessToken);
- 	console.log('refreshToken', refreshToken);
- 	console.log('profile', profile);
+ 	User.findOne({ googleId: profile.id }).then((existingUser) => {
+ 	 	if(existingUser){
+ 	 		//we already have a record for the profile id
+ 	 		done(null, existingUser);
+ 	 	}
+ 	 	else{
+ 	 		//we dont have a record for the profile id
+ 	 		new User({ googleId: profile.id }).save()
+ 	 		.then(user => done(null, user));
+ 	 	}
+ 	 });
+ 	
  })
 );
