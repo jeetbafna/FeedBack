8a24f8f21a7fb54b5cda03af47f992c84c4567fa
jeet bafna <jeetanil97@gmail.com>
2018-08-09 12:28:55 +0530

    Persisting form values

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,20 @@
+//Survey form review shows users their form inputs for reviews
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SurveyReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
 	return(
 		<div>
 		 <h5> Please confirm your entries</h5>
+		 <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+		 Back
+		 </button>
 		</div>
 	);
 };
 
-export default SurveyReview;
+function mapStateToProps(state) {
+	return{ formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
