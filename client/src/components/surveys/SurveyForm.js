2e04d0f64a73ac211ce803a7a646db9591962a5b
jeet bafna <jeetanil97@gmail.com>
2018-08-09 13:28:57 +0530

    Refactoring fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,12 @@ import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
 import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
-const FIELDS = [
- { label: 'Survey Title', name: 'title' },
- { label: 'Subject Line', name: 'subject' },
- { label: 'Email Body', name: 'body' },
- { label: 'Recipient List', name: 'emails'}
-];
 
 class SurveyForm extends Component {
 	renderFields() {
-		return _.map(FIELDS, ({ label, name}) => {
+		return _.map(formFields, ({ label, name}) => {
 			return( <Field key={name} component={SurveyField} type="text" label={label} name={name} />
 				);
 		});
@@ -43,7 +38,7 @@ function validate(values) {
 
 	errors.emails = validateEmails(values.emails || '');
 	
-	_.each(FIELDS, ({ name, label }) => {
+	_.each(formFields, ({ name, label }) => {
 		if(!values[name]) {
 			errors[name] = 'You must provide the '+ label;
 		}
